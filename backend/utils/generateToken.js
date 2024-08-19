0cb660f28e8ebfa1bc21732f8710fb06300ade81
aminebelho21 <belhocineamine21@gmail.com>
2024-08-19 18:56:33 +0100

    get user profile + update it

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (id) => {
-  console.log('JWT_SECRET:', process.env.JWT_SECRET); 
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: '30d',
   });
