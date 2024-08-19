63214235b37444b969b81a268fd04ac5773d7770
aminebelho21 <belhocineamine21@gmail.com>
2024-08-19 16:23:57 +0100

    Initial project setup with backend and frontend 2

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
