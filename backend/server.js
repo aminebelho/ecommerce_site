bf10ec434dfa881a3905d42f1cc3991aea2ce26e
aminebelho21 <belhocineamine21@gmail.com>
2024-08-19 16:26:52 +0100

    commit 1

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,8 @@ const app = express();
 
 // Basic route
 app.get('/', (req, res) => {
-  res.send('API is running...');
+  res.send('API is running...'); 
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
