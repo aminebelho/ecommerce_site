a70380258e7fbd1348aa272d16cf01f2b86399b6
aminebelho21 <belhocineamine21@gmail.com>
2024-08-23 03:03:04 +0100

    gr

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import HomeScreen from './pages/Home';
+import ProductScreen from './pages/ProductScreen';
+import CartScreen from './pages/CartScreen';
+import LoginScreen from './pages/LoginScreen';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Switch>
+        <Route path='/' component={HomeScreen} exact />
+        <Route path='/product/:id' component={ProductScreen} />
+        <Route path='/cart/:id?' component={CartScreen} />
+        <Route path='/login' component={LoginScreen} />
+      </Switch>
+    </main>
   );
-}
+};
 
 export default App;
